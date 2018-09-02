d2444282e53018eb58c4e4e9416374fb9a433226
purplerain <purplerain968@gmail.com>
2018-09-02 15:48:22 +0300

    P-1: Include `blog` urls in general urls file with namespace, add list and create view, provide templates for them.

--- portfolio_site/apps/blog/views.py
+++ portfolio_site/apps/blog/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.views.generic import ListView, CreateView
+from django.urls import reverse
 
-# Create your views here.
+from apps.blog.models import Post
+
+
+class ListPostView(ListView):
+    model = Post
+    template_name = 'post_list.html'
+
+
+class CreatePostView(CreateView):
+    model = Post
+    fields = ('title', 'content', 'is_draft', 'tags')
+    template_name = 'post_edit.html'
+
+    def get_success_url(self):
+        return reverse('blog:index')
