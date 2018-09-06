c72e89fa1b9fc80da87c5f243abefc91063e7398
purplerain <purplerain968@gmail.com>
2018-09-06 11:15:54 +0300

    P-2: Update gitignore, add settings for static files, implement utils functions to dynamically build static urls, add boostrap from `https://getbootstrap.com/docs/3.3/getting-started/`, add main template for blog, update blog templates.

--- portfolio_site/portfolio_site/settings/dev.py
+++ portfolio_site/portfolio_site/settings/dev.py
@@ -20,3 +20,5 @@ LOGGING = {
         }
     }
 }
+
+SERVE_MEDIA = True
--- portfolio_site/portfolio_site/urls.py
+++ portfolio_site/portfolio_site/urls.py
@@ -2,7 +2,11 @@ from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
 
+from portfolio_site.utils.utils import get_static_urls
+
 urlpatterns = [
     path('blog/', include('apps.blog.urls', namespace='blog')),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns = urlpatterns + get_static_urls()
