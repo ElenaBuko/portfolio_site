e1dd8a0e346303b0a4ced94b2f7aee8b00f7449b
purplerain <purplerain968@gmail.com>
2018-09-13 12:22:00 +0300

    P-2: Replace url patterns, update layout.

--- portfolio_site/apps/blog/urls.py
+++ portfolio_site/apps/blog/urls.py
@@ -7,8 +7,8 @@ app_name = 'blog'
 
 urlpatterns = [
     path(r'', ListPostView.as_view(), name='index'),
-    path(r'^create$', CreatePostView.as_view(), name='post-create'),
-    path(r'^edit/(?P<pk>\d+)/$', UpdatePostView.as_view(), name='post-edit'),
-    path(r'^delete/(?P<pk>\d+)/$', DeletePostView.as_view(), name='post-delete'),
-    path(r'^(?P<pk>\d+)/$', DetailPostView.as_view(), name='post-detail'),
+    path(r'create/', CreatePostView.as_view(), name='post-create'),
+    path(r'edit/<int:pk>/', UpdatePostView.as_view(), name='post-edit'),
+    path(r'delete/<int:pk>/', DeletePostView.as_view(), name='post-delete'),
+    path(r'<int:pk>/', DetailPostView.as_view(), name='post-detail'),
 ]
