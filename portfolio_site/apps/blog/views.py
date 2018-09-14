ae0bf59b06e497b629aaf2b3fded6721de6c9509
purplerain <purplerain968@gmail.com>
2018-09-14 14:55:31 +0300

    P-3: Add search implementation for post list view.

--- portfolio_site/apps/blog/views.py
+++ portfolio_site/apps/blog/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
 from django.urls import reverse
 
@@ -7,7 +8,19 @@ from apps.blog.models import Post
 class ListPostView(ListView):
     model = Post
     template_name = 'post_list.html'
+    context_object_name = 'posts'
     paginate_by = 2
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        query = self.request.GET.get('q')
+        if query:
+            queryset = queryset.filter(
+                Q(title__icontains=query) |
+                Q(content__icontains=query)
+            ).distinct()
+            self.paginate_by = None
+        return queryset
 
 
 class CreatePostView(CreateView):
